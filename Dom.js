e797781c24e5d00cd951d41772201e4175f29c54
shelter1120 <shelterk1120>
2022-12-08 16:02:16 +0530

    keyword

--- Dom.js
+++ Dom.js
@@ -57,3 +57,83 @@ for(var i=0; i <even.length;i++){
 
 }
 
+
+var itemList = document.getElementById("items")
+console.log(itemList);
+
+//parertElement
+console.log(itemList.parentElement);
+
+//lastelementchild
+console.log(itemList.lastElementChild);
+
+
+// lastchild
+console.log(itemList.lastChild);
+
+// createchild
+// console.log(createchild);
+
+// firstelementchild
+console.log(itemList.firstElementChild);
+
+// firstchild
+//  console.log(itemList.firstChild);
+
+//      //nextsibling
+//      console.log(itemList.nextSibling);
+
+//  console.log(itemList.nextelementsibling)
+
+// // previoussibling
+// console.log(itemList.previousSibling);
+
+// // previouselementsibling
+// console.log(itemList.previousElementSibling);
+
+//createElement
+
+var newDiv= document.createElement('div');
+
+newDiv.id='hello';
+newDiv.className='hello1';
+
+newDiv.setAttribute("title",'hello div')
+//text node
+var textNode= document.createTextNode("HEllo word")
+//add text to div
+newDiv.appendChild(textNode);
+
+//to add newDIv on Page
+
+var container = document.querySelector('header .container')
+var h1= document.querySelector('header h1')
+
+console.log(newDiv)
+ 
+newDiv.style.fontSize="40px";
+container.insertBefore(newDiv , h1)
+
+var newItem= document.createElement('div');
+
+newItem.id='item';
+newItem.className='item1';
+
+newItem.setAttribute("title",'item div')
+//text node
+var textNode= document.createTextNode("HEllo word")
+//add text to div
+newItem.appendChild(textNode);
+
+
+//to add newDIv on Page
+// first parent id/class and where we want to insert
+var container2 = document.querySelector('div #main')
+var list= document.querySelector('div #items')
+
+console.log(container2)
+ 
+newItem.style.fontSize="40px";
+container2.insertBefore(newItem , list)
+
+
