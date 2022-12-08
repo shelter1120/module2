98052b4710ce77313ce9486ccbe10b1abc83400a
shelter1120 <shelterk1120>
2022-12-08 09:53:08 +0530

    first Commit

---
+++ Dom.js
@@ -0,0 +1,32 @@
+// document.all[10].textContent="user list";
+// console.log(document.forms)
+
+//  var headerTitle= document.getElementById("header-title");
+// var header= document.getElementById("main-header");
+ 
+//getByClassName
+
+ 
+// var listItems= document.querySelectorAll(".list-group-item")
+// var list =document.getElementById("items")
+
+//  list.style.fontFamily="bold";
+
+// listItems[4].style.backgroundColor="green";
+
+// console.log(listItems)
+
+// headerTitle.textContent="Website Title"
+// headerTitle.innerText="user list"
+// header.style.borderBottom=" solid 3px #000"
+// var title= document.querySelector(".title")
+// title.classList.add("title")
+
+
+//getByTagName
+
+var tagName= document.getElementsByTagName('li')
+tagName[1].textContent='hello'
+tagName[4].style.backgroundColor="yellow"
+
+
