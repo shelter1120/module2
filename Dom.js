3453cb296befce09c986fc4f3f3a4e8f52c10e75
shelter1120 <shelterk1120>
2022-12-08 13:06:13 +0530

    changes using querySelctor & querySelctorAll

--- Dom.js
+++ Dom.js
@@ -7,12 +7,12 @@
 //getByClassName
 
  
-// var listItems= document.querySelectorAll(".list-group-item")
+//  var listItems= document.querySelectorAll(".list-group-item")
 // var list =document.getElementById("items")
 
 //  list.style.fontFamily="bold";
 
-// listItems[4].style.backgroundColor="green";
+// listItems.style.color="yellow";
 
 // console.log(listItems)
 
@@ -22,11 +22,38 @@
 // var title= document.querySelector(".title")
 // title.classList.add("title")
 
-
 //getByTagName
 
 var tagName= document.getElementsByTagName('li')
 tagName[1].textContent='hello'
 tagName[4].style.backgroundColor="yellow"
 
+//querySelector
+var input= document.querySelector("input")
+input.value="Type Here";
+
+var submit = document.querySelector('input[type="submit"]')
+submit.value="Send"
+
+var lastItem= document.querySelector('.list-group-item:nth-child(2)');
+lastItem.style.backgroundColor = "green";
+
+
+
+//querySelctorAll
+var secondItem= document.querySelectorAll(".list-group-item");
+secondItem[1].style.color = "green";
+
+var odd = document.querySelectorAll('li:nth-child(odd)')
+var even = document.querySelectorAll('li:nth-child(even)')
+
+
+for(var i=0; i <odd.length;i++){
+    odd[i].style.backgroundColor="lightpink"
+}
+
+for(var i=0; i <even.length;i++){
+    even[i].style.backgroundColor= "yellow"
+
+}
 
