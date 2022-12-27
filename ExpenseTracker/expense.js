83d8da0bebdfc9d1862d579e8037f196e3d267b2
shelter1120 <shelterk1120>
2022-12-27 22:21:32 +0530

    delete and edit network call

--- ExpenseTracker/expense.js
+++ ExpenseTracker/expense.js
@@ -1,80 +1,92 @@
- const form = document.getElementById('myForm');
- const msg = document.getElementById('message')
+const form = document.getElementById('myForm');
+const msg = document.getElementById('message')
 const userlist = document.getElementById('userList')
 
- const amount = document.getElementById('amount')
- const description = document.getElementById('description')
-const category= document.getElementById('category')
- form.addEventListener('submit',onSubmit)
+const amount = document.getElementById('amount')
+const description = document.getElementById('description')
+const category = document.getElementById('category')
+form.addEventListener('submit', onSubmit)
 
- function onSubmit(e){
+function onSubmit(e) {
     e.preventDefault()
-    if(amount.value =="" && description.value ==""  && category =="" ){
-        msg.innerHTML=" please enter details";
-        setTimeout(()=>{
+    if (amount.value == "" && description.value == "" && category == "") {
+        msg.innerHTML = " please enter details";
+        setTimeout(() => {
             msg.remove()
-        },4000)
-    } else{
-        const myobj ={
-            amount :amount.value,
-            description:description.value,
-            category:category.value
-            
+        }, 4000)
+    } else {
+        const myobj = {
+            amount: amount.value,
+            description: description.value,
+            category: category.value
+
         }
- 
-        axios.post("https://crudcrud.com/api/92f120125ee84432b1a4575346e2e34d/BookingApp",myobj)
-        .then((res)=>{
-            showUserOnScreen(res.data)
-             console.log(res)
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
+
+        axios.post("https://crudcrud.com/api/e8cde805fc3d46639d70851f36634b38/ExpenseApp", myobj)
+            .then((res) => {
+                showUserOnScreen(res.data)
+                console.log(res)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
 
         // localStorage.setItem(myobj.description,JSON.stringify(myobj))
         // showUserOnScreen(myobj)
-        amount.value="";
-        description.value="";
+        amount.value = "";
+        description.value = "";
         console.log(description.value)
     }
 
- }
+}
 
- window.addEventListener("DOMContentLoaded",()=>{
-     const data = axios.get("https://crudcrud.com/api/92f120125ee84432b1a4575346e2e34d/BookingApp")
-    .then((res)=>{
-        for(let i=0;i<res.data.length;i++){
-            showUserOnScreen(res.data[i])
-        }
- console.log(res)
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+window.addEventListener("DOMContentLoaded", () => {
+    const data = axios.get("https://crudcrud.com/api/e8cde805fc3d46639d70851f36634b38/ExpenseApp")
+        .then((res) => {
+            for (let i = 0; i < res.data.length; i++) {
+                showUserOnScreen(res.data[i])
+            }
+            console.log(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
     console.log(data)
- })
+})
 
 
- function showUserOnScreen(myobj){
-    const child = `<li id="${myobj.description}">${myobj.amount}-${myobj.description}-${myobj.category}
-    <button onClick='deleteItem("${myobj.description}")'> Delete</button>
-    <button onClick="editItem('${myobj.amount}','${myobj.description}','${myobj.category}')">Edit</button><li>`
- 
-    userlist.innerHTML=userlist.innerHTML + child;
+function showUserOnScreen(myobj) {
+    const child = `<li id="${myobj._id}">${myobj.amount}-${myobj.description}-${myobj.category}
+    <button onClick='deleteItem("${myobj._id}")'> Delete</button>
+    <button onClick="editItem('${myobj.amount}','${myobj.description}','${myobj.category}','${myobj._id}')">Edit</button><li>`
 
- }
+    userlist.innerHTML = userlist.innerHTML + child;
 
- function deleteItem(description){
-    localStorage.removeItem(description)
-    const child = document.getElementById(description)
+}
+
+function deleteItem(userId) {
+
+    axios.delete(`https://crudcrud.com/api/e8cde805fc3d46639d70851f36634b38/ExpenseApp/${userId}`)
+        .then((res) => {
+            // userlist.remove(res)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    localStorage.removeItem(userId)
+    const child = document.getElementById(userId)
     console.log(child);
+
     userlist.removeChild(child)
- }
- function editItem(amount,description,category){
-   //myobecj se value ara hai fir edit main again define
-    amount.value=amount;
-    description.value=description;
-    category.value=category;
-//button main call karte samya myobj.description use kiya then yha sirf description why?
-    deleteItem(description)
- }
+
+}
+function editItem(price, content, choice, userId) {
+    //myobecj se value ara hai fir edit main again define
+    amount.value = price;
+    description.value = content;
+    category.value = choice;
+    deleteItem(userId)
+    console.log(amount, description, category)
+    console.log(price, content, choice)
+
+}
